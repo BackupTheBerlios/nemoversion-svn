335883aafa229edfff853543e4a5b417353396fb
marcosmachado <marcosmachado@e640259a-e338-0410-83ff-e42d3441542b>
2007-12-28 00:48:25 +0000

    Testing remove command

--- trunk/src/nemoversion/handler.py
+++ trunk/src/nemoversion/handler.py
@@ -1,30 +1,51 @@
 #main module for nemo version
 import nautilus
 import gconf
+from nemoversion.appcontroller import factory
 
 class NemoversionHandler(nautilus.MenuProvider, nautilus.InfoProvider):
     def __init__(self):
         self.client = gconf.client_get_default()
         self.__menuItens = []
     
+    #TODO: remove
+    def __showInfo(self):
+        print self
+    
     def __addMenuItem(self, item, description):
+        """
+        Add a item to menuItens (itens for file-menu and bg-menu)
+        param item: menu label
+        param description: menu description
+        """
         menuid = 'Nemoversion::%s_item' %item.replace(' ', '_')
         item = nautilus.MenuItem(menuid, item, description)
-        item.connect('activate', self.menu_activate_cb, file)
+        item.connect('activate', self.__actionFor(file, menuid), file)
         self.__menuItens.append(item)
+    
+    #TODO: implement factory for __actionFor        
+    def __actionFor(self, file, menuid):
+        """
+        Dynamically get an action for the given file.
+        param file: a simple python file type
+        return: an activate action
+        """
+        actionFactory = factory.ActionFactory()
+        return actionFactory.createAction(file, menuId)
         
-    #TODO: implement
-    def menu_activate_cb(self, menu, file):
-        pass
-    
-    #TODO: implement    
-    def menu_background_activate_cb(self, menu, file):
-        pass
 
-    #TODO: implement
     def get_file_items(self, window, files):
-        pass
+        """
+        Callback function for files
+        param window:
+        param files: selected files
+        """
+        return self.__menuItens
     
-    #TODO: implement
     def get_background_items(self, window, file):
-        pass
+        """
+        Callback function for directory background click
+        param window:
+        param file: current dir background
+        """
+        return self.__menuItens
